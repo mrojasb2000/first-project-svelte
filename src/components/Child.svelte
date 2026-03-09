303eed995702e9b21d97114c1dda882ebfa9d85c
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-03-08 21:31:19 -0300

    chore: add basic new state count

--- src/components/Child.svelte
+++ src/components/Child.svelte
@@ -1,4 +1,13 @@
 <script lang='ts'>
+  // Defining props
   export let message:string;
+  // Defining state
+  let count: number = 0;
+
+  function increment() {
+    count += 1;
+  }
 </script>
 <h1>{message}</h1>
+<button on:click={increment}>Click me</button>
+<p>{count}</p>
