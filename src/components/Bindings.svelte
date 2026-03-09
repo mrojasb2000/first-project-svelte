7797b89690d2adf25e8b1b3eaf1072e4d21efc25
Rojas Barrientos, Mauricio (Externos - Amaris) <mauricio.rojasbarrientos@externos-cl.cencosud.com>
2026-03-08 22:36:17 -0300

    chore: add binding input with state

---
+++ src/components/Bindings.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let name:string = "John";
+</script>
+<input bind:value={name} />
+<p>{name}</p>
--- src/components/Parent.svelte
+++ src/components/Parent.svelte
@@ -1,6 +1,8 @@
 <script>
   import Child from './Child.svelte';
   import Counter from './Counter.svelte';
+  import Bindings from './Bindings.svelte';
 </script>
 <Child message="Hello World!" />
 <Counter />
+<Bindings />
